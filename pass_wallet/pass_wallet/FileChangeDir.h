e69224ea33b17c6eca22bcb183710d99b29cac28
snail_lk <544523660@qq.com>
2016-04-13 15:57:44 +0800

    练习文件目录操作
    为打包pkPass文件做准备

---
+++ pass_wallet/pass_wallet/FileChangeDir.h
@@ -0,0 +1,13 @@
+//
+//  FileChangeDir.h
+//  pass_wallet
+//
+//  Created by lk on 16/4/13.
+//  Copyright © 2016年 LK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileChangeDir : NSObject
+
+@end
---
+++ pass_wallet/pass_wallet/FileChangeDir.m
@@ -0,0 +1,13 @@
+//
+//  FileChangeDir.m
+//  pass_wallet
+//
+//  Created by lk on 16/4/13.
+//  Copyright © 2016年 LK. All rights reserved.
+//
+
+#import "FileChangeDir.h"
+
+@implementation FileChangeDir
+
+@end
