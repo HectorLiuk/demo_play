6774b3570cf8691794213886fc535f6fe860a3bb
snail_lk <544523660@qq.com>
2016-04-13 17:29:26 +0800

    生成文件夹类
    todo 如何打包pkpass

--- pass_wallet/pass_wallet/FileChangeDir.m
+++ pass_wallet/pass_wallet/FileChangeDir.m
@@ -8,6 +8,79 @@
 
 #import "FileChangeDir.h"
 
-@implementation FileChangeDir
+@interface FileChangeDir()
+@property (nonatomic, strong) NSFileManager *filemanage;
+@property (nonatomic, copy) NSString *docsdir;
+@end
 
+
+
+@implementation FileChangeDir
+- (NSFileManager *)filemanage{
+    if (!_filemanage) {
+        _filemanage = [NSFileManager defaultManager];
+    }
+    return _filemanage;
+}
+
+- (NSString *)docsdir{
+    if (!_docsdir) {
+        _docsdir = [NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    }
+    return _docsdir;
+}
+
++ (instancetype)fileDir{
+    return [[FileChangeDir alloc] init];
+}
+- (void)creatDirData:(NSData *)data images:(NSArray *)images blockPath:(void (^)(NSString * path))blockcall{
+    //创建文件夹
+    [self pathWithDirectoryName:nil];
+    //复制图片
+    for (NSString *imageName in images) {
+        [self copyImageToPass:imageName];
+    }
+    //注入pass.json
+    [self creatPassJsonData:data];
+    //回调文件地址
+    blockcall(self.docsdir);
+}
+
+
+//创建文件夹
+- (void)pathWithDirectoryName:(NSString *)directoryName{
+
+    if (directoryName == nil || directoryName.length == 0) {
+        self.docsdir = [self.docsdir stringByAppendingPathComponent:@"pass"];
+    } else {
+        self.docsdir = [self.docsdir stringByAppendingPathComponent:directoryName];
+    }
+    BOOL isDir;
+    BOOL exit =[self.filemanage fileExistsAtPath:self.docsdir isDirectory:&isDir];
+    if (!exit || !isDir) {
+        [self.filemanage createDirectoryAtPath:self.docsdir withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+}
+//创建pass.json文件
+- (void)creatPassJsonData:(NSData *)data{
+    NSString *dbpath = [self.docsdir stringByAppendingPathComponent:@"pass.json"];
+//    NSString *str = @"我是json数据";
+//    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
+    [self.filemanage createFileAtPath:dbpath contents:data attributes:nil];
+}
+//复制图片到文件夹下
+- (void)copyImageToPass:(NSString *)imageNamePng{
+    //调动复制方法必须 在设置toPath 文件详细地址
+    NSString *photoPath=[[NSBundle mainBundle] pathForResource:imageNamePng ofType:@"png"];
+    BOOL sucess =[self.filemanage copyItemAtPath:photoPath toPath:[NSString stringWithFormat:@"%@/%@.png",self.docsdir,imageNamePng] error:NULL];
+    if (sucess) {
+        NSLog(@"复制图片成功");
+    }else{
+        NSLog(@"复制图片失败");
+    }
+
+}
+- (void)deleteDir{
+    [self.filemanage removeItemAtPath:self.docsdir error:NULL];
+}
 @end
--- pass_wallet/pass_wallet/ViewController.m
+++ pass_wallet/pass_wallet/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 #import <PassKit/PassKit.h>
-
+#import "FileChangeDir.h"
 @interface ViewController ()<PKAddPassesViewControllerDelegate>
 @property (strong,nonatomic) PKPass *pass;//票据
 @property (strong,nonatomic) PKAddPassesViewController *addPassesController;//票据添加控制器
@@ -63,109 +63,28 @@
  }
 
 - (IBAction)becomeDir:(id)sender {
-    //创建文件路径
-    [self PathWithDirectoryName:@"pass"];
+    NSString *str = @"我是json数据";
+    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
+    NSArray *array = @[@"111",@"222"];
     
+    FileChangeDir *file = [FileChangeDir fileDir];
     
+    [file creatDirData:data images:array blockPath:^(NSString * path) {
+        NSLog(@"parh:%@",path);
+        //打包pkpass方法
+
+    }];
+    [file deleteDir];
+
     
 }
 - (IBAction)pkpass:(id)sender {
-    //------------------创建文件/文件夹
-    //获取沙盒目录
-    NSString *homePath = NSHomeDirectory();
-    //在沙盒目录中创建一个文件file.text
-    NSString *filePath = [homePath stringByAppendingPathComponent:@"Documents/file.text"];
-    //NSFileManager是单利模式,所以不能使用alloc+init创建
-    NSFileManager *manager = [NSFileManager defaultManager];
-    NSString *str = @"无线互联";
-    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
-    //参数：文件路径、文件内容、文件的属性
-    BOOL sucess = [manager createFileAtPath:filePath contents:data attributes:nil];
-    if(sucess){
-        NSLog(@"文件创建成功");
-    }else{
-        NSLog(@"文件创建失败");
-    }
-    
-    //创建文件夹
-    NSString *filePaths = [homePath stringByAppendingPathComponent:@"Documents/file"];
-    NSError *error;
-    //需要传递一个创建失败的指针对象，记录创建失败的信息
-    BOOL success1 = [manager createDirectoryAtPath:filePaths withIntermediateDirectories:YES attributes:nil error:&error];
-    if(!success1){
-        NSLog(@"创建成功");
-    }else{
-        NSLog(@"创建失败");
-    }
-    
-    
-    //--------------------读取文件
-    //根据路径读取文件内容
-    NSData *datas = [manager contentsAtPath:filePath];
-    NSString *s = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    NSLog(@"%@",s);
-    
-    
-    //--------------------移动文件/剪切文件
-    //NSFileManager中没有提供重命名的方法，所以我们可以借助移动的api进行操作
-    //把filePath移动到targetPath目录中
-    NSString *targetPath = [homePath stringByAppendingPathComponent:@"Documents/file/file2.text"];
-    BOOL sucess2 = [manager moveItemAtPath:filePath toPath:targetPath error:nil];
-    if(sucess2) {
-        NSLog(@"移动成功");
-    }else{
-        NSLog(@"移动失败");
-    }
     
 }
 
 - (IBAction)addWallet:(id)sender {
      [self addPass];
 }
-
-
-
-
-- (NSString *)PathWithDirectoryName:(NSString *)directoryName
-{
-    NSString *docsdir = [NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-    NSString *json = [NSString stringWithFormat:@"%@/test.json",docsdir];
-
-    NSFileManager *filemanage = [NSFileManager defaultManager];
-    
-    if (directoryName == nil || directoryName.length == 0) {
-        docsdir = [docsdir stringByAppendingPathComponent:@"pass"];
-    } else {
-        docsdir = [docsdir stringByAppendingPathComponent:directoryName];
-    }
-    BOOL isDir;
-    BOOL exit =[filemanage fileExistsAtPath:docsdir isDirectory:&isDir];
-    if (!exit || !isDir) {
-        [filemanage createDirectoryAtPath:docsdir withIntermediateDirectories:YES attributes:nil error:nil];
-    }
-    //创建pass.json文件
-    
-    NSString *dbpath = [docsdir stringByAppendingPathComponent:@"pass.json"];
-    NSString *str = @"我是json数据";
-    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
-    [filemanage createFileAtPath:dbpath contents:data attributes:nil];
-    
-    
-    //复制图片到dir中
-    //调动复制方法必须 在设置toPath 文件详细地址
-    NSString *photoPath=[[NSBundle mainBundle] pathForResource:@"111" ofType:@"png"];
-    BOOL sucess =[filemanage copyItemAtPath:photoPath toPath:[NSString stringWithFormat:@"%@/111.png",docsdir] error:NULL];
-    if (sucess) {
-        NSLog(@"成功");
-    }else{
-        NSLog(@"失败");
-    }
-    
-    
-    return docsdir;
-}
-
-
 
 
 
