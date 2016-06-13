df7c093c20e9e0aa03f0843ee02d6ac29349dac6
snail_lk <544523660@qq.com>
2016-06-13 17:56:39 +0800

    swift_customView

---
+++ CustomView(Swift)/CustomView(Swift)/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  CustomView(Swift)
+//
+//  Created by lk on 16/6/13.
+//  Copyright © 2016年 LK. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    //懒加载创建控件
+    private lazy var backImageView: customView = {
+        let backImageView = customView()
+        return backImageView
+    }()
+    let i:Int
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(backImageView)
+        UIView.animateWithDuration(10) { () -> Void in
+            
+        }
+        
+        backImageView.operationBlock = {(str) -> Void in
+            
+            let oo = Int()
+            
+            
+            print(String(oo)+"11111"+str)
+            
+
+
+            print(self,self.view,oo)
+            
+        }
+//
+        //使用正常创建控件
+//        let ImageView = customView.init(frame: CGRectMake(10, 300, 300, 200))
+//        view.addSubview(ImageView)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
---
+++ CustomView(Swift)/CustomView(Swift)/customView.swift
@@ -0,0 +1,72 @@
+//
+//  customView.swift
+//  CustomView(Swift)
+//
+//  Created by lk on 16/6/13.
+//  Copyright © 2016年 LK. All rights reserved.
+//
+
+import UIKit
+/// 自定义swift控件
+class customView: UIView {
+
+    @IBOutlet weak var sureBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    
+    //block
+    var operationBlock: ((str:String) -> Void)?
+    
+    private lazy var view: UIView = {
+        let view = NSBundle.mainBundle().loadNibNamed("customView", owner: self, options: nil)[0]as!UIView
+        return view
+    }()
+    
+    override init (frame:CGRect){
+        super.init(frame: frame)
+        self.view.backgroundColor = UIColor.redColor()
+        setUpSubviews()
+        
+    }
+
+    convenience init() {
+        self.init(frame: UIScreen.mainScreen().bounds)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpSubviews()
+        
+    }
+    
+    func fromeNibView() ->UIView {
+        let nib = NSBundle.mainBundle().loadNibNamed("customView", owner: self, options: nil)
+        return nib[0]as!UIView
+
+    }
+    
+    func setUpSubviews() {
+        self.backgroundColor = UIColor.init(white: 0.0, alpha: 0.3)
+        view.backgroundColor = UIColor.redColor()
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleHeight,UIViewAutoresizing.FlexibleWidth]
+        view.center = self.center
+        addSubview(view)
+        
+    }
+    
+    func removeCurrentView() {
+       removeFromSuperview()
+    }
+    
+    
+    @IBAction func sureClick(sender: AnyObject) {
+        removeCurrentView()
+        if operationBlock != nil {
+            operationBlock!(str: "0000000")
+        }
+    }
+    
+    @IBAction func cancelClick(sender: AnyObject) {
+        removeCurrentView()
+
+    }
+}
